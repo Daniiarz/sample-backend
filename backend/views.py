eec1cd038454b392174e9e6f6a9e8b728447a0f0
daniiar <daniyarflash.m01@gmail.com>
2022-06-29 19:03:10 +0600

    Fixed issues

--- backend/views.py
+++ backend/views.py
@@ -3,7 +3,7 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework import status
 from rest_framework.authtoken.models import Token
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, GenericAPIView
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.status import (
@@ -43,9 +43,9 @@ class ListLessonMarkAPIView(ListCreateAPIView):
         return Response(status=status.HTTP_201_CREATED)
 
 
-@csrf_exempt
 @api_view(["POST"])
 @permission_classes((AllowAny,))
+@csrf_exempt
 def login(request):
     username = request.data.get("username")
     password = request.data.get("password")
